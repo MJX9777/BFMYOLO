21b44ebb158a7fd8dd746b0e1dad5f5beb9c66cc
MJX9777 <106296184+MJX9777@users.noreply.github.com>
2024-03-26 15:41:30 +0800

    Update test.py

--- test.py
+++ test.py
@@ -345,7 +345,7 @@ def test(data,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='test.py')
-    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/MFA/weights/best.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='', help='model.pt path(s)')
     parser.add_argument('--data', type=str, default='data/SRvedai.yaml', help='*.data path')
     parser.add_argument('--batch-size', type=int, default=1, help='size of each image batch')
     parser.add_argument('--img-size', type=int, default=512, help='inference size (pixels)')
